67830c372f25ddd11fe2f5bdd6175b423d978a0f
Danial Al Ghazali Walangadi <danialagw@students.unnes.ac.id>
2024-02-29 07:02:34 +0700

    Added push_forward()

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
  * Tugas Minggu-3 : Singly Linked List
  * Buatlah sebuah node yang merepresentasikan mahasiswa yang berisi data mahasiswa:
  * nim bertipe integer dan nama bertipe string. Acuan data adalah nim.
- * Buat operasi (tambah data) linked list, (print data), insert data, dan delete data
+ * Buat operasi (tambah data) linked list, (print data), (insert data), dan delete data
  */
 
 
@@ -18,7 +18,7 @@ struct SinglyLinkedList{
     Node* head = nullptr;
     Node* tail = nullptr;
 
-    void push_back(const int& id, const std::string& name){
+    void push_back(int id, const std::string& name){
         Node* temp = new Node();
         temp -> nama = name;
         temp -> nim = id;
@@ -28,6 +28,20 @@ struct SinglyLinkedList{
         } else{
             tail -> next = temp;
             tail = temp;
+        }
+        tail -> next = nullptr;
+    }
+
+    void push_forward(int id, const std::string& name){
+        Node* temp = new Node();
+        temp -> nama = name;
+        temp -> nim = id;
+        if(head == nullptr){
+            head = temp;
+            tail = temp;
+        } else{
+            temp -> next = head;
+            head = temp;
         }
         tail -> next = nullptr;
     }
@@ -92,6 +106,6 @@ int main(){
     list.push_back(143, "Danial");
     list.push_back(163, "Dicky");
     list.push_back(130,"Antonio");
-    list.insert_after(130,142,"Yusuf");
+    list.push_forward(142,"Yusuf");
     list.transverse();
 }
